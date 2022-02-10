75061002125deb37e0a9969ac9f1bdeb270cfa20
amstell27 <sergei_i27@mail.ru>
2022-02-10 13:21:18 +0700

    add Class

---
+++ src/test/java/guru/qa/FirstSimpleTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("Это наш первый простой тест")
+    
+    public class FirstSimpleTest {
+
+    @DisplayName("Простенький тест на assertEquals")
+    @Test
+    void firstTest() {
+
+        Assertions.assertEquals(1,1);
+    }
+}
