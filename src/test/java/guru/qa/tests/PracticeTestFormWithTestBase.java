1a83fea298695693eac887570bb306c6a7cd23bb
amstell27 <sergei_i27@mail.ru>
2022-02-18 13:06:19 +0700

    add TestBase

--- src/test/java/guru/qa/tests/PracticeTestFormWithTestBase.java
+++ src/test/java/guru/qa/tests/PracticeTestFormWithTestBase.java
@@ -1,36 +1,29 @@
 package guru.qa.tests;
 
-import com.codeborne.selenide.Configuration;
+
 import com.codeborne.selenide.selector.ByText;
 import guru.qa.pages.RegistrationPage;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class PracticeTestForm {
+public class PracticeTestFormWithTestBase extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     String firstName = "Alex";
     String lastName = " Alexeev";
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-    }
+
 
     @Test
     void automationPracticeTests() {
 
         registrationPage.openPage();
-        registrationPage
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setBirthDate("26", "June", "1992");
+        registrationPage.setFirstName(firstName).setLastName(lastName).setBirthDate("26", "June", "1992");
         $("#lastName").setValue("Alexeev");
         $("#userEmail").setValue("emain@email.com");
 
@@ -56,9 +49,9 @@ public class PracticeTestForm {
 
         $("[class='modal-header']").shouldBe(text("Thanks for submitting the form"));
 
- //       $$(".table-responsive tr").filterBy(text("Student Name")).shouldBe(texts("Alex Alexeev"));
- //       $$(".table-responsive tr").filterBy(text("Student Email")).shouldBe(texts("emain@email.com"));
- //       $$(".table-responsive tr").filterBy(text("Gender")).shouldBe(texts("Male"));
+        //       $$(".table-responsive tr").filterBy(text("Student Name")).shouldBe(texts("Alex Alexeev"));
+        //       $$(".table-responsive tr").filterBy(text("Student Email")).shouldBe(texts("emain@email.com"));
+        //       $$(".table-responsive tr").filterBy(text("Gender")).shouldBe(texts("Male"));
         $$(".table-responsive tr").filterBy(text("Mobile")).shouldBe(texts("8002000500"));
         $$(".table-responsive tr").filterBy(text("Date of Birth")).shouldBe(texts("25 June,1992"));
         $$(".table-responsive tr").filterBy(text("Subjects")).shouldBe(texts("Chemistry"));
@@ -67,10 +60,7 @@ public class PracticeTestForm {
         $$(".table-responsive tr").filterBy(text("Address")).shouldBe(texts("Current Address"));
         $$(".table-responsive tr").filterBy(text("State and City")).shouldBe(texts("NCR Delhi"));
 
-        registrationPage
-                .checkForm("Student Name", firstName + lastName)
-                .checkForm("Gender", "Male")
-                .checkForm("Student Email", "emain@email.com");
+        registrationPage.checkForm("Student Name", firstName + lastName).checkForm("Gender", "Male").checkForm("Student Email", "emain@email.com");
 
     }
 }
